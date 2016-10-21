b206b7b1a84e3ca34e4f9a63a6da0c63b166b107
irisfaraway <iris.faraway@gmail.com>
2016-10-21 22:47:52 +0100

    make it purty with foundation

--- bin/app.rb
+++ bin/app.rb
@@ -2,11 +2,9 @@ require 'sinatra'
 require './lib/random-sims/random.rb'
 
 set :port, 8080
-set :static, true
-set :public_folder, 'static'
 set :views, 'views'
-enable :sessions
-set :session_secret, 'BADSECRET'
+set :public, 'public'
+set :static, true
 
 get '/' do
   @sims = Randomiser.generate_sims
